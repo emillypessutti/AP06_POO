e40d8b3f2d597c0a65f6fc9b343f8927cad0266b
emillypessutti <101837818+emillypessutti@users.noreply.github.com>
2025-06-06 16:42:44 -0300

    Ajuste na estrutura das pastas.

--- CatalogoDeProdutos/obj/Debug/net9.0/CatalogoDeProdutos.AssemblyInfo.cs
+++ CatalogoDeProdutos/obj/Debug/net9.0/CatalogoDeProdutos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogoDeProdutos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a8cd860a2d889c29d1f9951c15353d62e0a3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogoDeProdutos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogoDeProdutos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
